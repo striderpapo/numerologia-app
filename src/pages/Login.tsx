5e10e9f7766f38bb0f60e642c08087dd2f47df01
striderpapo <78055747+striderpapo@users.noreply.github.com>
2026-07-22 14:55:01 -0600

    Primer commit - Numerología

---
+++ src/pages/Login.tsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  /*const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log({
+      email,
+      password
+    });
+
+    // temporalmente entramos al Home
+    navigate("/home");
+  };*/
+
+  const handleSubmit = (e: React.FormEvent) => {
+  e.preventDefault();
+
+  const correoCorrecto = "admin@numerologia.com";
+  const passwordCorrecto = "123456";
+
+
+  if (email === correoCorrecto && password === passwordCorrecto) {
+
+    navigate("/home");
+
+  } else {
+
+    alert("Correo o contraseña incorrectos");
+
+  }
+};
+
+
+  return (
+    <div className="login-container">
+
+      <div className="login-card">
+
+        <h1>Numerología</h1>
+
+        <h2>Iniciar sesión</h2>
+
+
+        <form onSubmit={handleSubmit}>
+
+          <div>
+            <label>
+              Correo
+            </label>
+
+            <input
+              type="email"
+              placeholder="correo@ejemplo.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+          </div>
+
+
+          <div>
+
+            <label>
+              Contraseña
+            </label>
+
+            <input
+              type="password"
+              placeholder="Contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+          </div>
+
+
+          <button type="submit">
+            Ingresar
+          </button>
+
+
+        </form>
+
+      </div>
+
+    </div>
+  );
+}
